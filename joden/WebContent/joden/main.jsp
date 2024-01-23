7ec98b71acc7412bd87988e749cd4b52e21728ec
itwill <itwill@DESKTOP-IKG5IMI>
2024-01-23 20:09:46 +0900

    1/23 20:00

--- joden/WebContent/joden/main.jsp
+++ joden/WebContent/joden/main.jsp
@@ -13,40 +13,52 @@ String cp = request.getContextPath();
 <meta charset="UTF-8">
 <title>Insert title here</title>
 
+<!-- material icon -->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 <link rel="stylesheet" type="text/css" href="<%=cp %>/joden/css/style.css"/>
-<link rel="stylesheet" type="text/css" href="<%=cp %>/joden/css/main.css"/>
+<link rel="stylesheet" type="text/css" href="<%=cp %>/joden/css/style_header.css"/>
 
 </head>
 <body>
 
-<div id="main">
+<!-- header 전체 -->
+<div id="header">
 
-<div>
-카테고리
-</div>
+<!-- 로그인,회원가입 버튼 디자인 -->
+	<div id="header-first">
+		<a class="header-join">회원가입</a>
+			<div id="header-first-line"></div>
+		<a class="header-login">로그인</a>
+	</div>
 
-<div>
-로고
-</div>
+<!-- 검색창,로고 전체 -->
+	<div id="header-secound">
+	
+<!-- 카테고리 -->
+		<div id="header-secound-main">
+			<div id="header-secound-category">
+				<img alt="" src="*/image/category.png" class="header-secound-category-img">
+				<span class="header-secound-category-text">카테고리</span>
+			</div>
+		</div>
+		
+		<div id="header-secound-logo">
+		<a href="<%=cp%>/cabin/main.gos">
+				<img alt="" src="*/image/category.png" class="header-secound-category-img"></a>
+		</div>
 
-<div>
-검색
-
-</div>
-
-<div id="main_title">
-shop
-</div>
-
-
+		<form action="" method="post" id="headerSearchForm">
+		<div id="headerSearchForm_box">
+		<input name="searchProduct" placeholder="상품명 검색"
+						class="headerSearchForm-input"/>
+		<button class="headerSearchForm-btn">
+		<i class="tiny material-icons">search</i></button>
+		</div>
+		</form>
 
 
-
-
-
-
-
-
+	</div>
 
 
 </div>
--- joden/src/com/gos/GosServlet3.java
+++ joden/src/com/gos/GosServlet3.java
@@ -1,5 +1,6 @@
 package com.gos;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
@@ -14,6 +15,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.gosutil.DBConnection;
 import com.gosutil.MyUtil;
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 public class GosServlet3 extends HttpServlet{
 
@@ -43,57 +46,80 @@ public class GosServlet3 extends HttpServlet{
 		String uri = req.getRequestURI();
 		String url;
 		
-		if(uri.indexOf("main.gos")!=-1) {
+		String root = getServletContext().getRealPath("/");
+		String path = root+"imageBoard"+File.separator+"saveImage";
+		
+		File f = new File(path);
+		if(!f.exists()) {
+			f.mkdirs();
+		}
+		
+		if(uri.indexOf("write.gos")!=-1) {
 			
-//			String pageNum = req.getParameter("pageNum");
-//			int currentPage = 1;
-//			
-//			if(pageNum!=null) {
-//				currentPage = Integer.parseInt(pageNum);
-//			}
-//			
-//			String searchKey = req.getParameter("searchKey");
-//			String searchValue = req.getParameter("searchValue");
-//			
-//			if(searchValue==null) {
-//				searchKey = "category";
-//				searchValue = "";
-//			}else {
-//				if(req.getMethod().equalsIgnoreCase("GET")) {
-//					searchValue = URLDecoder.decode(searchValue,"UTF-8");
-//				}
-//			}
-//			
-//			int totaldataCount = dao.totalDataCount(searchKey, searchValue);
-//			
-//			int numPerPage = 5;
-//			int totalPage = myUtil.getPageCount(numPerPage,totaldataCount);
-//			
-//			if(currentPage>totalPage) {
-//				currentPage=totalPage;
-//			}
-//			
-//			int start = (currentPage-1)*totalPage+1;
-//			int end = currentPage*totalPage;
-//			
-//			List<GosDTO3> lists = dao.getProductInfoList(start, end, searchKey, searchValue);
-//			
-//			String param = "";
-//			if(searchValue!=null && !searchValue.equals("")) {
-//				param = "searchKey="+searchKey+"&searchValue="+URLEncoder.encode(searchValue,"UTF-8");
-//			}
-//			
-//			String urlList = cp+"/cabin/main.gos";
-//			if(!param.equals("")) {
-//				urlList +="?"+param;
-//			}
-//			
-//			String pageIndexList = myUtil.pageIndexList(currentPage, totalPage, urlList);
-//			
-//			req.setAttribute("lists", lists);
-//			req.setAttribute("pageNum", currentPage);
-//			req.setAttribute("pageIndexList", pageIndexList);
-//			req.setAttribute("totaldataCount", totaldataCount);
+			url = "/joden/writh.jsp";
+			forward(req, resp, url);
+			
+		}if(uri.indexOf("write_ok_gos")!=-1){
+			
+			String enctype="UTF-8";
+			int maxSize = 10*1024*1024;
+			
+			MultipartRequest mr = new MultipartRequest(req, path,maxSize,enctype, new DefaultFileRenamePolicy());
+			
+			url=cp+"/joden/list.gos";
+			resp.sendRedirect(url);
+		
+		}else if(uri.indexOf("main.gos")!=-1) {
+			
+			String pageNum = req.getParameter("pageNum");
+			int currentPage = 1;
+			
+			if(pageNum!=null) {
+				currentPage = Integer.parseInt(pageNum);
+			}
+			
+			String searchKey = req.getParameter("searchKey");
+			String searchValue = req.getParameter("searchValue");
+			
+			if(searchValue==null) {
+				searchKey = "category";
+				searchValue = "";
+			}else {
+				if(req.getMethod().equalsIgnoreCase("GET")) {
+					searchValue = URLDecoder.decode(searchValue,"UTF-8");
+				}
+			}
+			
+			int totaldataCount = dao.totalDataCount(searchKey, searchValue);
+			
+			int numPerPage = 5;
+			int totalPage = myUtil.getPageCount(numPerPage,totaldataCount);
+			
+			if(currentPage>totalPage) {
+				currentPage=totalPage;
+			}
+			
+			int start = (currentPage-1)*totalPage+1;
+			int end = currentPage*totalPage;
+			
+			List<GosDTO3> lists = dao.getProductInfoList(start, end, searchKey, searchValue);
+			
+			String param = "";
+			if(searchValue!=null && !searchValue.equals("")) {
+				param = "searchKey="+searchKey+"&searchValue="+URLEncoder.encode(searchValue,"UTF-8");
+			}
+			
+			String urlList = cp+"/cabin/main.gos";
+			if(!param.equals("")) {
+				urlList +="?"+param;
+			}
+			
+			String pageIndexList = myUtil.pageIndexList(currentPage, totalPage, urlList);
+			
+			req.setAttribute("lists", lists);
+			req.setAttribute("pageNum", currentPage);
+			req.setAttribute("pageIndexList", pageIndexList);
+			req.setAttribute("totaldataCount", totaldataCount);
 //			
 			url = "/joden/main.jsp";
 			forward(req, resp, url);
